dec36ea4879f9e6a6d7e23f3e657422e91faec3f
Kawan <kawan-silva@outlook.com>
2023-08-23 17:52:44 -0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr max-content',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
