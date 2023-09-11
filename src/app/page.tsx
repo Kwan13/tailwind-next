01b2fe5a36fdb2e3626212156cba6e2c4ea03858
Kawan <kawan-silva@outlook.com>
2023-09-11 17:54:19 -0300

    componentizando o Select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { SettingsTabs } from '@/components/SettingsTabs';
 import { InputRoot, InputPrefix, InputControl } from '@/components/Input';
 import * as FileInput from '@/components/Form/FileInput';
 import { Select } from '@/components/Form/Select';
+import { SelectItem } from '@/components/Form/Select/SelectItem';
 
 export default function Home() {
   return (
@@ -112,7 +113,10 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -122,7 +126,13 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div />
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
